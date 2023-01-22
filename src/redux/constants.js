3a515d59009f19d48717f914ee6e9fc2169bbef2
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2023-01-22 15:04:56 +0200

    add constatns

---
+++ src/redux/constants.js
@@ -0,0 +1,4 @@
+export const dataForm = {
+  name: '',
+  number: '',
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,23 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
+import { dataForm } from './constants';
 // Початкове значення стану Redux для кореневого редюсера,
 // якщо не передати параметр preloadedState.
-const initialState = {
+const configureStore = {
   contacts: [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-],
-  filter: '',
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filter: {
+    name: dataForm.name,
+    number: dataForm.number,
+  },
 };
 // Поки що використовуємо редюсер який
 // тільки повертає отриманий стан
-const rootReducer = (state = initialState, action) => {
+const rootReducer = (state = configureStore, action) => {
   return state;
 };
 
